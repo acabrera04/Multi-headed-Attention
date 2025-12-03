e478ff3ba42f6d4fd4d9174934462976efc311c2
Avanish Kulkarni <bigfatnotebook@gmail.com>
2025-12-02 20:25:03 -0500

    update load_tokens to be includable

--- src/load_tokens.c
+++ src/load_tokens.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "load_tokens.h"
 
-int main() {
-    FILE *f = fopen("../work/tokens.bin", "rb");
+int* load_tokens(const char* filename, int* num_tokens) {
+    FILE *f = fopen(filename, "rb");
+    if (!f) {
+        printf("failed to open tokens file: %s\n", filename);
+        return EXIT_FAILURE;
+    }
 
     // get file size (token is int)
     fseek(f, 0, SEEK_END);
     long file_size = ftell(f);
     fseek(f, 0, SEEK_SET);
 
-    int num_tokens = file_size / sizeof(int);
-    printf("tokens.bin is %d tokens long\n", num_tokens);
+    *num_tokens = file_size / sizeof(int);
 
-    int *tokens = (int *)malloc(num_tokens * sizeof(int));
+    int *tokens = (int *)malloc(*num_tokens * sizeof(int));
 
-    size_t read_count = fread(tokens, sizeof(int), num_tokens, f);
-    if (read_count != num_tokens) {
-        fprintf(stderr, "Error reading tokens: expected %d, got %zu\n", num_tokens, read_count);
+    size_t read_count = fread(tokens, sizeof(int), *num_tokens, f);
+    if (read_count != (size_t)*num_tokens) {
+        fprintf(stderr, "Error reading tokens: expected %d, got %zu\n", *num_tokens, read_count);
         free(tokens);
         fclose(f);
         return EXIT_FAILURE;
     }
 
-    printf("tokens: [");
-    for (int i = 0; i < num_tokens; i++) {
-        printf("%d", tokens[i]);
-        if (i < num_tokens - 1) {
-            printf(", ");
-        }
-    }
-    printf("]\n");
-
-    free(tokens);
     fclose(f);
-    return EXIT_SUCCESS;
-
-    // TODO return the tokens in a function
+    return tokens;
 }
---
+++ src/load_tokens.h
@@ -0,0 +1,6 @@
+#ifndef LOAD_TOKENS_H
+#define LOAD_TOKENS_H
+
+int* load_tokens(const char* filename, int* num_tokens);
+
+#endif // LOAD_TOKENS_H
