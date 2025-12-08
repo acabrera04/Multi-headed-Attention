bb3ced67b0d4de39023704bb4cb006edc352fadc
acabrera04 <acabrera04164@gmail.com>
2025-12-08 00:11:03 -0500

    mpi cuda init

--- src/mpi_cuda_attention.cpp
+++ src/mpi_cuda_attention.cpp
@@ -1,24 +1,35 @@
-/*
-  Allen Cabrera, Avanish Kulkarni
-  This file will contain the driver to run the CUDA kernels on the device
-*/
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433832
+#endif
 
-#include <cuda_runtime.h>
+#include "load_tokens.h"
+#include "model.h"
+#include <math.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "model.h"
+#include <string.h>
+#include <sys/time.h>
+#include <cuda_runtime.h>
 #include "../include/cuda_utils.h"
-#include "load_tokens.h"
-
-#define NUM_STEPS 100
-#define N_LAYER 12
 
 int *load_tokens(const char *filename, int *num_tokens);
 GPT2Model *load_model(const char *filename);
 void free_model(GPT2Model *model);
 int inference(GPT2Model *model, int *tokens, int num_tokens, int k, int *top_indices, float *top_scores);
-int main()
+int main(int argc, char **argv)
 {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size > N_HEAD)
+    {
+        if (rank == 0)
+            printf("Warning: Number of ranks is greater than N_HEAD (12) and some ranks will not be used.\n");
+    }
     // Initialize - start CUDA device, load model, allocate memory, create current state
     // Read arg1 for input
     const char *model_path = "./gpt2_124m.bin";
@@ -36,13 +47,24 @@ int main()
     }
 
     // Input Processing - encode/tokenize input text, get tokens
-    tokens = load_tokens(tokens_path, &num_tokens);
-    if (!tokens)
-    {
-        printf("Failed to load tokens\n");
-        free_model(model);
-        return 1;
+    if (rank == 0) {
+        tokens = load_tokens(tokens_path, &num_tokens);
+        printf("rank 0 has loaded %d tokens\n", num_tokens);
+        if (!tokens) {
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
     }
+
+    MPI_Bcast(&num_tokens, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    printf("Rank %d: numtokens: %d\n", rank, num_tokens);
+
+    if (rank != 0) {
+        tokens = (int *)malloc(num_tokens * sizeof(int));
+    }
+
+    MPI_Bcast(tokens, num_tokens, MPI_INT, 0, MPI_COMM_WORLD);
+    
     // call inference in cuda
     inference(model, tokens, num_tokens, k, top_indices, top_scores);
 
@@ -61,5 +83,7 @@ int main()
     // Free memory
     free(tokens);
     free_model(model);
+    MPI_Finalize();
+
     return 0;
 }
